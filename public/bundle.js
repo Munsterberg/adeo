c32b10552f49179e6ddcfb85c32ef72ec0c05e21
Munsterberg <jetrussell93@gmail.com>
2016-08-08 18:37:46 -0400

    create main component and wire it to App.jsx

--- public/bundle.js
+++ public/bundle.js
@@ -54,13 +54,13 @@
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 
+	var _main = __webpack_require__(175);
+
+	var _main2 = _interopRequireDefault(_main);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	_reactDom2.default.render(_react2.default.createElement(
-	  'h1',
-	  null,
-	  'Hello'
-	), document.getElementById('app') // eslint-disable-line
+	_reactDom2.default.render(_react2.default.createElement(_main2.default, null), document.getElementById('app') // eslint-disable-line
 	);
 
 /***/ },
@@ -21468,6 +21468,36 @@
 	var ReactMount = __webpack_require__(167);
 
 	module.exports = ReactMount.renderSubtreeIntoContainer;
+
+/***/ },
+/* 175 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	var Main = function Main() {
+	  return _react2.default.createElement(
+	    'div',
+	    null,
+	    _react2.default.createElement(
+	      'h1',
+	      null,
+	      'Main Component 1'
+	    )
+	  );
+	};
+
+	exports.default = Main;
 
 /***/ }
 /******/ ]);
