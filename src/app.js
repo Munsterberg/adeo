96392c8e6cbee83ef1b075fd9ad3696c26b36b6c
Munsterberg <jetrussell93@gmail.com>
2016-08-08 17:40:10 -0400

    Create App Structure 	* install eslint, express, babel, and body-parser 	* configure eslintrc 	* configure express server and babel

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get('/', (req, res) => {
+  res.send('Hola');
+});
+
+app.use((err, req, res, next) => {
+  console.log('unhandled application error: ', err);
+  res.status(500).send(err);
+});
+
+export default app;
